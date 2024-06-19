4543ff059eb740ba4d4a908996b2c654bf1ada07
Matt <solomatias321@gmail.com>
2024-06-19 20:01:11 +0200

    wip orders details by user

--- src/app/webhooks/stripe/route.tsx
+++ src/app/webhooks/stripe/route.tsx
@@ -1,9 +1,11 @@
 import db from "@/db/db";
-import { create } from "domain";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from "resend";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   const event = stripe.webhooks.constructEvent(
@@ -26,15 +28,29 @@ export async function POST(req: NextRequest) {
       email,
       orders: { create: { productId, pricePaidInCents } },
     };
-    
-    const {orders: [order]} = await db.user.upsert({ where: { email }, create: userFields, update: userFields,
-    select:{ orders: {orderBy: { createdAt: "desc" }, take: 1 }} });
+
+    const {
+      orders: [order],
+    } = await db.user.upsert({
+      where: { email },
+      create: userFields,
+      update: userFields,
+      select: { orders: { orderBy: { createdAt: "desc" }, take: 1 } },
+    });
 
     const downloadVerification = await db.downloadVerification.create({
-        data: {
-            productId,
-            expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24)
-        }
-    })
+      data: {
+        productId,
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      },
+    });
+
+    await resend.emails.send({
+      from: `Support <${process.env.SENDER_EMAIL}>`,
+      to: email,
+      subject: "Order Configuration",
+      react: <h1> Hi </h1>,
+    });
   }
+  return new NextResponse();
 }
