8ca91462a37f071c3df92349aaed1668f8296c62
Matt <solomatias321@gmail.com>
2024-06-24 11:44:25 +0200

    wip: oders page

--- src/email/components/OrderInformation.tsx
+++ src/email/components/OrderInformation.tsx
@@ -1,5 +1,12 @@
 import { formatCurrency } from "@/lib/formatters";
-import { Column, Row, Section, Text, Img } from "@react-email/components";
+import {
+  Column,
+  Row,
+  Section,
+  Text,
+  Img,
+  Button,
+} from "@react-email/components";
 
 type OrderInformationProps = {
   order: { id: string; createdAt: Date; pricePaidInCents: number };
@@ -52,12 +59,21 @@ export function OrderInformation({
           <Column className="align-bottom">
             <Text className=" text-lg font-bold m-0 mr-4">{product.name}</Text>
           </Column>
+          <Column align="right">
+            <Button
+              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/products/download/${downloadVerificationId}`}
+              className="bg-black text-white px-6 py-4 rounded text-lg"
+            >
+              Download
+            </Button>
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <Text className="text-gray-500 mb-0">{product.description}</Text>
+          </Column>
         </Row>
       </Section>
     </>
   );
-}
-
-export function TestRebase() {
-  return <h1>Rebasing</h1>;
 }
